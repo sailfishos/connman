38d50e0fac7c8d440bea016fdcc41f2779cfe89a
Jussi Laakkonen <jussi.laakkonen@jolla.com>
2024-11-29 18:12:19 +0200

    [wifi] Use old get_tethering() format. TODO: squash in appropriate commit

--- connman/plugins/wifi.c
+++ connman/plugins/wifi.c
@@ -3377,6 +3377,13 @@ static GSupplicantSSID *ssid_ap_init(const char *ssid, const char *passphrase)
 	if (!ap)
 		return NULL;
 
+
+	bool ret = connman_technology_get_wifi_tethering(&ssid, &passphrase);
+	if (ret == false) {
+		g_free(ap);
+		return NULL;
+	}
+
 	ap->mode = G_SUPPLICANT_MODE_MASTER;
 	ap->ssid = ssid;
 	ap->ssid_len = strlen(ssid);
