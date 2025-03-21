11ff2960614027cf6d0aedfe76f2d64e5d8ece28
Jussi Laakkonen <jussi.laakkonen@jolla.com>
2025-03-21 12:34:03 +0200

    wireguard: Add the transport nameservers for DNS reresolve query
    [wireguard] Add the transport nameservers for DNS reresolve query. JB#63273
    
    Use the nameservers of the VPN transport with the GResolv queries done
    after the initial connection to reresolve the endpoint address.

--- connman/vpn/plugins/wireguard.c
+++ connman/vpn/plugins/wireguard.c
@@ -53,6 +53,7 @@
 
 #define DNS_RERESOLVE_TIMEOUT 20
 #define DNS_RERESOLVE_ERROR_LIMIT 5
+#define DNS_DEFAULT_PORT 53
 #define ROUTE_SETUP_TIMEOUT 200 // ms
 #define ARRAY_SIZE(a) (sizeof(a)/sizeof(a[0]))
 
@@ -533,7 +534,7 @@ static void resolve_endpoint_cb(GResolvResultStatus status,
 	case G_RESOLV_RESULT_STATUS_NO_ANSWER:
 	case G_RESOLV_RESULT_STATUS_NO_RESPONSE:
 	case G_RESOLV_RESULT_STATUS_SERVER_FAILURE:
-		DBG("retry DNS reresolve");
+		DBG("retry DNS reresolve, status %d", status);
 		if (info->provider)
 			vpn_provider_add_error(info->provider,
 					VPN_PROVIDER_ERROR_CONNECT_FAILED);
@@ -545,7 +546,7 @@ static void resolve_endpoint_cb(GResolvResultStatus status,
 	case G_RESOLV_RESULT_STATUS_FORMAT_ERROR:
 	case G_RESOLV_RESULT_STATUS_NOT_IMPLEMENTED:
 	case G_RESOLV_RESULT_STATUS_REFUSED:
-		DBG("stop DNS reresolve, error %d", status);
+		DBG("stop DNS reresolve, status %d", status);
 		if (err && info->provider)
 			vpn_provider_add_error(info->provider,
 					VPN_PROVIDER_ERROR_CONNECT_FAILED);
@@ -599,7 +600,9 @@ static int disconnect(struct vpn_provider *provider, int error);
 static gboolean wg_dns_reresolve_cb(gpointer user_data)
 {
 	struct wireguard_info *info = user_data;
+	char **nameservers;
 	int err;
+	int i;
 
 	DBG("");
 
@@ -618,8 +621,11 @@ static gboolean wg_dns_reresolve_cb(gpointer user_data)
 
 	DBG("endpoint_fqdn %s", info->endpoint_fqdn);
 
-	vpn_util_resolv_add_nameserver(info->resolv, "::1", 53, 0);
-	vpn_util_resolv_add_nameserver(info->resolv, "127.0.0.1", 53, 0);
+	nameservers = vpn_provider_get_string_list(info->provider,
+							"TransportNameservers");
+	for (i = 0; nameservers && nameservers[i]; i++)
+		vpn_util_resolv_add_nameserver(info->resolv, nameservers[i],
+							DNS_DEFAULT_PORT, 0);
 
 	info->resolv_id = vpn_util_resolve_hostname(info->resolv,
 						info->endpoint_fqdn,
